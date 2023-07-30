875d44b83385304a356338078059926bc2b45a48
Stendhalsynd <qmflf556@naver.com>
2023-07-30 22:05:02 +0900

    cors 서버 활성화 설정

--- src/main/java/com/example/AutomateX/AutomateXApplication.java
+++ src/main/java/com/example/AutomateX/AutomateXApplication.java
@@ -2,6 +2,9 @@ package com.example.AutomateX;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AutomateXApplication {
@@ -10,4 +13,17 @@ public class AutomateXApplication {
 		SpringApplication.run(AutomateXApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/**") // 특정 엔드포인트에 대한 CORS 설정
+						.allowedOrigins("http://localhost:3000") // 프론트엔드 서버 주소
+						.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+						.allowCredentials(true) // 쿠키 인증 요청 허용
+						.maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트 캐싱
+			}
+		};
+	}
 }
