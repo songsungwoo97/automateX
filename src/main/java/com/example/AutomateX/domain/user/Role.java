7a862b640bfee2d02af152fa071637c5a863de60
Stendhalsynd <qmflf556@naver.com>
2023-09-05 19:45:09 +0900

    Role 한글 변경

--- src/main/java/com/example/AutomateX/domain/user/Role.java
+++ src/main/java/com/example/AutomateX/domain/user/Role.java
@@ -12,7 +12,7 @@ public enum Role {
   USER("ROLE_USER", "일반 사용자", false),
   ADMIN("ROLE_ADMIN", "어드민", true),
   ULHMC("ROLE_ULHMC", "울산항만공사", true),
-  OPERATOR1("ROLE_OPERATOR1", "신흥사", false),
+  신흥사("ROLE_OPERATOR1", "신흥사", false),
   OPERATOR2("ROLE_OPERATOR2", "고려항만", false),
   OPERATOR3("ROLE_OPERATOR3", "훙산항만운영", false),
   OPERATOR4("ROLE_OPERATOR4", "한국보팔터미날", false);
